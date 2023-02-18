41f7f128f6c7da632dc9c99b8b77ffe0a1b73c56
LunaLlijen <110740322+LunaLlijen@users.noreply.github.com>
2023-02-17 23:09:26 -0800

    Update background color

--- navbar.js
+++ navbar.js
@@ -3,7 +3,7 @@ import './styles.css';
 function NavBar(){
   return(
     <>
-    <nav className="navbar navbar-expand-lg navbar-light bg-light" >
+    <nav className="navbar navbar-expand-lg navbar-light bg-primary" >
       <a className="navbar-brand" href="#">BadBank</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" aria-current="page">
         <span className="navbar-toggler-icon"></span>
